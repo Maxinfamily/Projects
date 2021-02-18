652414de265e564d9618a2caaf797221b99ec278
Carlos Bernal <22770674+Maxinfamily@users.noreply.github.com>
2021-02-18 11:09:31 +0000

    Cleaning and reorganizing projects

--- Test/BattleshipLite/BattleshipLite.Tests/UnitTest1.cs
+++ Test/BattleshipLite/BattleshipLite.Tests/UnitTest1.cs
@@ -14,5 +14,43 @@ namespace BattleshipLite.Tests
         {
             Assert.Pass();
         }
+
+        [Test]
+        public void BasicEndGame()
+        {
+            Game currentGame = new Game();
+
+            // Define a test input and output value:
+            bool expectedResult = true;
+            Boat battleship = new Boat()
+            {
+                this.length = 5,
+                this.position = new Point(1,1),
+                this.hits = 5,
+                this.status = sunken;
+            };
+
+            Boat destroyerA = new Boat()
+            {
+                this.length = 4,
+                this.position = new Point(1, 1),
+                this.hits = 5,
+                this.status = sunken;
+            };
+
+            Boat destroyerB = new Boat()
+            {
+                this.length = 4,
+                this.position = new Point(1, 1),
+                this.hits = 5,
+                this.status = sunken;
+            };
+
+            // Run the method under test:
+            double actualResult = rooter.SquareRoot(input);
+            // Verify the result:
+            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 100);
+
+        }
     }
 }
